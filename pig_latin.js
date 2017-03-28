101ad3ae5bb8d63fc1849331c356c795d4dc9d99
Bill Ivan <billivank_96@yahoo.co.id>
2017-03-28 22:35:51 +0700

    pig-latin finished

--- pig_latin.js
+++ pig_latin.js
@@ -4,5 +4,27 @@
 const readline = require('readline');
 
 function pigLatin(sentence) {
-  // Your pig latin implementation here...
+  var pl="";
+  var vow = /[aeiou]/gi;
+  if (sentence[0].match(vow)){
+  return sentence;
+} else {
+  var con=sentence.indexOf(sentence.match(vow)[0]);
+  pl=sentence.substr(con) + sentence.substr(0,con) + "ay";
 }
+return pl;
+}
+
+var rl = readline.createInterface(process.stdin, process.stdout);
+var recursiveAsyncReadLine = function(){
+  rl.question("What would you like to piglatinise?", function(wor){
+    wor=wor.split(" ");
+    var res=[];
+      for(var i=0; i<wor.length; i++){
+        res.push(pigLatin(wor[i]));
+      }
+    console.log(res.join(" "));
+    recursiveAsyncReadLine();
+  });
+};
+recursiveAsyncReadLine();
