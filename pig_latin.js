52ed1a5ec162708e50e88a41201a65f0f94734bc
1000piar <nunua7a8@gmail.com>
2017-03-29 21:07:51 +0700

    pig_Latin.js

--- pig_latin.js
+++ pig_latin.js
@@ -2,7 +2,32 @@
 
 //use readline to fix this challenge
 const readline = require('readline');
+const rl = readline.createInterface({
+input : process.stdin,
+output : process.stdout,
+prompt : "HAI please input your word to process pigLatin>"
+});
+
+rl.prompt();
+rl.on('line', (answer) => {
+pigLatin(answer);
+  });
+
 
 function pigLatin(sentence) {
-  // Your pig latin implementation here...
-}
+  var pigLatin = '';
+    var regex = /[aeiou]/gi;
+    if (sentence[0].match(regex)) {
+      pigLatin = sentence;
+    } else {
+      var vowelIndice = sentence.indexOf(sentence.match(regex)[0]);
+      pigLatin = sentence.substr(vowelIndice) + sentence.substr(0, vowelIndice) + 'ay.';
+    }
+    // return pigLatin;
+    console.log(pigLatin);
+  }
+
+  function splitSentence(sentence){
+    var Newsentence = pigLatin.split(' ').length;
+      console.log(splitSentence(Newsentence));
+  }
