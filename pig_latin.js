cd7770b82745e0c30b1789708639233d5abdcbcc
zanisis <adzannyaa@gmail.com>
2017-03-29 11:47:34 +0700

    adzanny done

--- pig_latin.js
+++ pig_latin.js
@@ -1,8 +1,41 @@
-"use strict"
-
+// "use strict"
 //use readline to fix this challenge
 const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'INPUT>>'
+})
 
-function pigLatin(sentence) {
-  // Your pig latin implementation here...
+rl.prompt();
+
+rl.on('line', (line) => {
+    console.log(kalimat(line))
+    rl.prompt();
+});
+
+function kalimat(input) {
+  if (input === '') {
+    return 'Error';
+  } else {
+    input = input.split(' ');
+    for (let i = 0; i < input.length; i++) {
+      input[i] = satuKata(input[i]);
+    }
+    return input.join(' ');
+  }
+  rl.prompt()
+}
+
+function satuKata(input) {
+  let pattern = /[aiueo]/gi;
+  let ditemukan = input.search(pattern);
+  if (ditemukan > 0) {
+    return input.slice(ditemukan) + '' + input.slice(0, ditemukan) + 'ay';
+  } else {
+    if (ditemukan === 0) {
+      return input;
+    }
+    return "Error";
+  }
 }
