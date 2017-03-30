4f8b5a014c8dcadb79d029fa3d1130ba3ce7a246
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-03-30 17:23:56 +0700

    Revisi Pig Latin

--- pig_latin.js
+++ pig_latin.js
@@ -4,19 +4,19 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: 'Masukkan Input> '
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Masukkan Input> '
 });
 
 rl.prompt();
 
 rl.on('line', (line) => {
-  convertKata(line.trim());
-  rl.prompt();
+    convertKata(line.trim());
+    rl.prompt();
 }).on('close', () => {
-  console.log('Have a great day!');
-  process.exit(0);
+    console.log('Have a great day!');
+    process.exit(0);
 });
 
 function convertKata(kata) {
@@ -26,25 +26,35 @@ function convertKata(kata) {
     for (var i = 0; i < temp_sentence.length; i++) {
         str = str + pigLatin(temp_sentence[i]) + ' ';
     }
-    
+
     console.log(str);
 }
 
 function pigLatin(sentence) {
-  // Your pig latin implementation here...
-let pola_vokal = /[aiueo]/gi;
-    let newStr;
-    
-    for (var i = 0; i < sentence.length; i++) {
-        if (sentence[i].match(pola_vokal)) {
-            newStr = sentence + 'yay';
-            break;
-        } else {
-            let konsonan = sentence[i];
-            newStr = sentence.slice(1) + konsonan + 'ay';
-            break;
-        }
+    // Your pig latin implementation here...
+    let pola_vokal = /[aiueo]/gi;
+    let pola_konsonan = /[^aiueo]/gi;
+    let newStr = "";
+    let konsonan = '';
+    let vokal = '';
+    let kalimat = '';
+
+    let temp_sentence = sentence.split('');
+
+    for(var i = 0; i < temp_sentence.length; i++){
+      if(temp_sentence[0].match(pola_vokal)){
+        newStr = sentence;
+        break;
+      }
+
+      if(temp_sentence[i].match(pola_konsonan)){
+        konsonan += temp_sentence[i];
+      } else {
+        newStr = sentence.slice(i) + konsonan + 'ay';
+        break;
+      }
     }
+
     return newStr;
 
 }
