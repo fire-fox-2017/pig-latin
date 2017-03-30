fda713247e358d692044219fc0511b3814d0eed8
aldyadk <aldy.andika@gmail.com>
2017-03-30 19:44:35 +0700

    komit pig latin

--- pig_latin.js
+++ pig_latin.js
@@ -3,6 +3,40 @@
 //use readline to fix this challenge
 const readline = require('readline');
 
-function pigLatin(sentence) {
-  // Your pig latin implementation here...
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '> '
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {
+  let inputSentence = line.split(' ');
+  var outputSentence = '';
+  for (let i=0 ; i < inputSentence.length ; i++){
+    outputSentence += `${pigLatin(inputSentence[i])} `;
+  }
+  console.log(outputSentence.trim());
+  rl.prompt();
+}).on('close', () => {
+  process.exit(0);
+});
+
+function pigLatin(sentence){
+  let temp = '';
+  let wordSlice = sentence.slice(sentence.search(/[aiueo]/i));
+
+  for(let i=0 ; i<sentence.length ; i++){
+    if(/[^aiueo]/i.test(sentence[i])){
+      temp += sentence[i];
+    } else
+      break;
+  }
+
+  if(/[aiueo]/i.test(sentence[0])){
+    return wordSlice;
+  } else {
+    return wordSlice + temp + 'ay'
+  }
 }
